88f180e5de7155eb9666229fdef85e503072c043
MacBookAirM1 <github@acc.stntwk.net>
2023-02-21 14:51:44 +0900

    20230221

--- src/main/java/net/stntwk/springmvcmodelsample/MainController.java
+++ src/main/java/net/stntwk/springmvcmodelsample/MainController.java
@@ -1,20 +1,27 @@
 package net.stntwk.springmvcmodelsample;
 
+import net.stntwk.springmvcmodelsample.MainForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
-public class MainController {
+public class MainController{
     @GetMapping("showinputform")
-    public String showInputForm(){
+    public String showInputForm(Model m){
+        List<String> fruits = Arrays.asList("りんご", "ぶどう", "みかん", "すいか", "いちご");
+        m.addAttribute("fruits", fruits);
         return "ShowInputForm";
     }
 
     @PostMapping("confirmdata")
-    public String confirmData(Model model, MainForm form){
-        model.addAttribute("form", form);
+    public String confirmData(Model m, MainForm f){
+        m.addAttribute("form", f);
         return "ConfirmData";
     }
 }
--- src/main/java/net/stntwk/springmvcmodelsample/MainForm.java
+++ src/main/java/net/stntwk/springmvcmodelsample/MainForm.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @Data
-public class MainForm {
+public class MainForm{
     private String name;
-    private Integer age;
+    private int age;
+    private String gender;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthday;
+    private String favoriteColor;
+    private String favoriteFruit;
 }
